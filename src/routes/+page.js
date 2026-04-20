7cb44e49745f099de08bcf495c7aac53e672fe5e
jack-walk <jacktmwalker@gmail.com>
2026-04-20 19:16:25 -0400

    Loading map and importing tree data

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,11 @@
 // Page settings
 // These values are passed to the layout to control what appears on the page.
+import greatTrees from '$lib/data/great-trees.json';
+
 export function load() {
   return {
-    // Set to false to hide the NYCity News Service header
     showHeader: true,
-    // Set to false to hide the site footer
     showFooter: true,
+    greatTrees,
   };
 }
