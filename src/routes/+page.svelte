10893f098dbc4876c6ed6827265a4b8b9f904a9c
jack-walk <jacktmwalker@gmail.com>
2026-04-20 20:29:51 -0400

    Adding legend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import Map from '$lib/components/Maps/Map.svelte';
   import MapLayer from '$lib/components/Maps/MapLayer.svelte';
   import Geocoder from '$lib/components/Maps/Geocoder.svelte';
+  import Legend from '$lib/components/Maps/Legend.svelte';
 
   let { data } = $props();
   const greatTrees = data.greatTrees;
@@ -15,19 +16,29 @@
 
 <div class="container">
   <ArticleHeader
-    headline="The Great Trees of New York"
+    headline="Where to find the London plane, New York's most common tree"
     byline="NYCity News Service"
     pubDate="2026-04-20"
   />
 
   <p>
-    In 1985, the New York City Parks Department began an ambitious new venture.
-    They called it the Great Tree Search.
+    Every ten years, thousands of volunteers canvass the city's street trees,
+    recording their species, trunk diameter, health, and exact location. In the
+    2015 survey, they found more than 600,000 trees and 133 different species.
   </p>
 
   <p>
-    The agency called on the public to nominate "trees of unusual size, species,
-    form or historical association." Explore them here and see if you can find any near you.
+    One tree dominates the count: the London plane. It was the tree favored by
+    Robert Moses, the unelected "power broker" who controlled city parks for
+    decades in the mid 20th century. He made sure the hearty hybrid was planted
+    everywhere in New York, where it can be found in every borough and on nearly
+    every block.
+  </p>
+
+  <p>
+    According to the 2015 census, there are more than 87,000 London plane
+    trees on New York's streets, 12% of the city's population of street trees.
+    Here's where the most London planes are found:
   </p>
 
   <Geocoder
@@ -83,5 +94,35 @@
       }}
     />
   </Map>
+
+  <Legend
+    title="London plane trees"
+    mode="threshold"
+    items={[
+      {
+        color: '#eef2f9',
+        to: 146,
+      },
+      {
+        color: '#c2cfe6',
+        from: 147,
+        to: 268,
+      },
+      {
+        color: '#8098cc',
+        from: 269,
+        to: 433,
+      },
+      {
+        color: '#3366b3',
+        from: 434,
+        to: 696,
+      },
+      {
+        color: '#0033a1',
+        from: 697,
+      },
+    ]}
+  />
 
 </div>
